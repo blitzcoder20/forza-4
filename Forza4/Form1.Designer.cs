193655e127191db75cf05760d31039e4bb2cb490
blitzcoder20 <67243785+blitzcoder20@users.noreply.github.com>
2023-01-13 17:39:54 +0100

    Project upload

---
+++ Forza4/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Forza4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(134, 106);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 300);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(134, 40);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(350, 67);
+            this.panel2.TabIndex = 0;
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(269, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Forza 4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(638, 406);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Forza4/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Forza4.Properties;
+
+namespace Forza4
+{
+    public partial class Form1 : Form
+    {
+        int indiceSelezione = 0;
+        string[,] celle = new string[6,7];
+        bool chi = true;
+        int indice;
+       
+        int contamosse=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PianoGioco();
+            PianoSelezione();
+
+        }
+        void PianoGioco()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    PictureBox pcb = new PictureBox();
+                    pcb.SetBounds(j * 50 + 5, i * 50 + 5, 40, 40);
+                    pcb.BackColor = Color.Transparent;
+                    pcb.Image = Resources.Grigio;
+
+                    pcb.SizeMode = PictureBoxSizeMode.StretchImage;
+                    panel1.Controls.Add(pcb);
+                    celle[i,j] = null;
+
+                }
+            }
+            panel1.BackColor = Color.Blue;
+        }
+        void PianoSelezione()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                PictureBox pcb = new PictureBox();
+                pcb.SetBounds(i * 50 + 5, 10, 40, 40);
+                pcb.BackColor = Color.White;
+
+                pcb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pcb.BackColor = Color.Transparent;
+                panel2.Controls.Add(pcb);
+            }
+
+            DisegnaPiano();
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+
+          
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                if (indiceSelezione != 6)
+                {
+                    
+                    indiceSelezione++;
+                    DisegnaPiano();
+
+                }
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                if (indiceSelezione != 0)
+                {
+                    indiceSelezione--;
+                    DisegnaPiano();
+                }
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                DisegnaGioco();
+                DisegnaPiano();
+
+            }
+        }
+        void DisegnaPiano()
+        {
+
+            for (int i = 0; i < 7; i++)
+            {
+                if (i == indiceSelezione)
+                {
+                    PictureBox pic = panel2.Controls[indiceSelezione] as PictureBox;
+                    if (chi)
+                    {
+                        pic.Image = Resources.rosso;
+                    }
+                    else
+                    {
+                        pic.Image = Resources.giallo;
+                    }
+
+                }
+                else
+                {
+                    PictureBox pic = panel2.Controls[i] as PictureBox;
+                    pic.Image = Resources.Grigio;
+                }
+            }
+        }
+        void DisegnaGioco()
+        {
+            string colore;
+             indice = TrovaRiga();
+            int indicePannello = indice * 7 + indiceSelezione;
+            PictureBox pcb = panel1.Controls[indicePannello] as PictureBox;
+            pcb.SizeMode = PictureBoxSizeMode.StretchImage;
+            if (celle[0,indiceSelezione] != null)
+            {
+                return;
+            }
+            if (chi == true)
+            {
+                pcb.Image = Resources.rosso;
+                colore = "rosso";
+            }
+            else
+            {
+                pcb.Image = Resources.giallo;
+                colore = "giallo";
+            }           
+            chi = !chi;
+            celle[indice,indiceSelezione] = colore;
+            if (contamosse >= 6)
+            {
+                bool vinto = Vittoria(indice, indiceSelezione, colore);
+                if (vinto)
+                {
+                    MessageBox.Show("ha vinto " + colore);
+                    Resetta();
+                }
+                if (Riempito())
+                {
+                    MessageBox.Show("Nessuno ha vinto");
+                    Resetta();
+                }
+            }
+            indiceSelezione = 0;
+            contamosse++;
+           }
+        bool Vittoria(int riga,int colonna,string colore)
+        {
+                    
+            int conta = 0;
+            //orizzontale
+            for (int i = 0; i < celle.GetLength(1); i++)
+            {
+                if (celle[riga, i] == colore)
+                {
+                    conta++;
+                }
+                else conta = 0;
+                if (conta >= 4)
+                    return true;
+            }
+            //verticale
+            conta = 0;
+            for (int i = 0; i < celle.GetLength(0); i++)
+            {
+                if (celle[i, colonna] == colore)
+                {
+                    conta++;
+                }
+                else conta = 0;
+                if (conta >= 4)
+                    return true;
+            }
+            //obliquo sinistra
+            conta = 0;
+            for (int i = -4; i < 5; i++)
+            {
+               
+                
+                    if (riga+i>=0 && colonna+i>=0 && riga+i<=5 && colonna+i<=6 )
+                    {
+                        if (celle[riga + i, colonna + i] == colore)
+                        {
+                            conta++;
+                        }
+                        else conta = 0;
+                        if (conta >= 4)
+                            return true;
+                    }
+                }
+            //obliquo destro
+            conta = 0;
+            for (int i = -4; i < 5; i++)
+            {
+
+
+               if (riga + i >= 0 && colonna - i >= 0 && riga +i <= 5 && colonna - i <= 6)
+                {
+                    if (celle[riga + i, colonna - i] == colore)
+                    {
+                        conta++;
+                    }
+                    else conta = 0;
+                    if (conta >= 4)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+        int TrovaRiga()
+        {
+          
+            int riga = 0;
+       
+            for (int i = 0; i <celle.GetLength(0)-1; i ++)
+            {
+
+               if (celle[i+1, indiceSelezione] == null)
+                {
+                    riga++;
+
+                }
+                
+            }
+
+            return riga;
+        }
+        bool Riempito()
+        {
+
+            for(int i=0;i<celle.GetLength(0);i++)
+            {
+                for (int j = 0; j < celle.GetLength(1); j++)
+                {
+                    if (celle[i, j] == null)
+                    {
+                        return false;
+                    }
+                }
+
+            }
+            return true;
+        }
+       void Resetta()
+        {
+            for(int i = 0; i < panel1.Controls.Count; i++)
+            {
+                PictureBox pcb = panel1.Controls[i] as PictureBox;
+                pcb.Image = Resources.Grigio;
+            }
+            for(int j = 0; j < celle.GetLength(0); j++)
+            {
+                for(int i = 0; i < celle.GetLength(1); i++)
+                {
+                    celle[j, i] = null;
+                }
+            }
+        }
+        
+    }
+}
+
+    
+
